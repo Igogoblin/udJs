af75b8df46d2e8a20f74a819428c6ad60c8c9b89
Igo <igogoblin@gmail.com>
2020-04-20 20:39:52 +0300

    first task

--- forLook.js
+++ forLook.js
@@ -1,1 +1,23 @@
-console.log("prov");
+
+var money = prompt("Ваш бюджет на месяц?","");
+console.log(money);
+var time = prompt("YYYY-MM-DD","");
+console.log(time);
+
+var appData={
+budger:money,
+timeData : time,
+expenses:{},
+optionalExpenses:{},
+income:[],
+savings:false
+};
+let ask1 = prompt("Введите обязательную статью расходов в этом месяце","");
+let ask2 = prompt("Во сколько обойдется?","");
+
+appData.expenses[ask1]=ask2;
+
+
+    alert(appData.budger/30);
+
+
